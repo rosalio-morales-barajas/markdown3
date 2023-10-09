d27e861c640f0bfdff4782eaa45b4e6fbbfc403a
RosalioMorales1975 <chaliomorales46@gmail.com>
2023-10-08 23:29:34 -0600

    Full project

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState } from  'react';
+import { marked } from  'marked';
+import './App.css';
+
+// header (H1 size), a sub header (H2 size), a link, 
+// inline code, a code block, a list item, 
+// a blockquote, an image, and bolded text
+
+function App() {
+
+  const [text, setText] = useState(`
+   #  H1
+   ## H2
+   [title](https://www.example.com)
+   \`code\`
+   \`\`\`
+   {
+     "firstName": "John",
+     "lastName": "Smith",
+     "age": 25
+   }
+   \`\`\`
+   
+   - First item
+   - Second item
+   - Third item
+
+   > blockquote
+
+   ![alt text](image.jpg)
+
+   **bold text**
+
+  `);
+
+  marked.setOptions ({
+    breaks: true
+  })
+  
+
+  return (
+    <div className="App">
+     <textarea id="editor" 
+     onChange={(event) => { 
+      setText(event.target.value); 
+      }} 
+      value = {text}
+      ></textarea>
+     <div 
+     id="preview" 
+     dangerouslySetInnerHTML={{
+      __html: marked(text),
+     }} 
+     ></div>
+    </div>
+  );
+}
+
+export default App;
